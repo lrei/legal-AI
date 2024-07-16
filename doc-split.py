800240b12df08f5f9d4d44ef295cd921c3cfdb58
makov3c <makovecbarbara1@gmail.com>
2024-07-16 15:48:09 +0200

    not-working-yet

--- doc-split.py
+++ doc-split.py
@@ -20,79 +20,70 @@ def extract_text_from_pdf(pdf_path):
 
     return text
 
-extracted_text = extract_text_from_pdf(pdf_path)
-
-print(extracted_text)
-
-output_path = "data/extracted_text.txt"
-
-with open(output_path, 'w', encoding='utf-8') as file: # encoding mora biti na windowsu
-    file.write(extracted_text)
-
-    with open(output_path, 'r', encoding='utf-8') as file:
-        text = file.read()
+def split(text):
 
     matches = re.finditer(r'\nArticle (1[0-1][0-3]|[1-9][0-9]?|1[0-9]{2})\n.+\n', text)
     positions = [match.start() for match in matches]
 
-    # print(positions)
+    print("POSITIONS: ", positions)
 
     sections = []
     start = 0
 
     for pos in positions:
-        section = extracted_text[start:pos]
+        section = text[start:pos]
         sections.append(section)
         start = pos
 
-    # Add the last section from the last position to the end of the text
-    sections.append(extracted_text[start:])
+    sections.append(text[start:])
 
+
+    # splittajmo še introduction torej [0:249423]
+    matches2 = re.finditer(r'\(\b(1[0-8][0-9]|[1-9]?[0-9])\b\)\n', text[:249422])
+
+    positions2 = [match.start() for match in matches2]
+    start = 0
+
+    for pos in positions2:
+        section = text[start:pos]
+        sections.append(section)
+        start = pos
+
+    # itak moramo vse nespremenjeno appendat. Zdaj lahko samo overwritamo vse, zato [start:]
+    sections.append(text[start:249422]) 
     separator = "\n---\n".join(sections)
-
-    sections_path = "data/sections.txt"
-
-    with open(sections_path, 'w', encoding='utf-8') as file:
-        file.write(separator)
     
+    print("LENGTH: ", len(sections))
+    print(sections[34])
+
+    return sections
+
+def generate_json(sections):
+    print("Pritning from generate_json function")
+    print(sections[34])
+    return 0
+
+    # Generating JSON
     with open(sections_path, 'r', encoding='utf-8') as file:
-        text2 = file.read()
-        # splittajmo še introduction torej [0:249423]
-        matches2 = re.finditer(r'\(\b(1[0-8][0-9]|[1-9]?[0-9])\b\)\n', text2[:249422])
+        content = file.read()
 
-        positions2 = [match.start() for match in matches2]
-        print(len(positions2), "\n")
-        #splittamo po pos2
+    blocks = content.split('\n---\n')
+    # creating a list of JSON objects
+    objects = []
+    for pos, block in enumerate(blocks):
+        object = {
+            "text": block,
+            "position": pos
+        }
+        objects.append(object)
 
-        sections = []
-        start = 0
+    with open('data/output.json', 'w') as json_file:
+        json.dump(objects, json_file, indent=4)
 
-        for pos in positions2:
-            section = text2[start:pos]
-            sections.append(section)
-            start = pos
 
-        # itak moramo vse nespremenjeno appendat. Zdaj lahko samo overwritamo vse, zato [start:]
-        sections.append(text2[start:]) 
-        separator = "\n---\n".join(sections)
-    
-    with open(sections_path, 'w', encoding='utf-8') as file:
-        file.write(separator)
 
-all_positions = positions2 + positions
-# Generating JSON
-with open(sections_path, 'r', encoding='utf-8') as file:
-    content = file.read()
 
-blocks = content.split('\n---\n')
-# creating a list of JSON objects
-objects = []
-for pos, block in enumerate(blocks):
-    object = {
-        "text": block,
-        "position": pos
-    }
-    objects.append(object)
+if __name__ == "__main__":
+    text = extract_text_from_pdf(pdf_path)
+    generate_json(split(text))
 
-with open('data/output.json', 'w') as json_file:
-    json.dump(objects, json_file, indent=4)
